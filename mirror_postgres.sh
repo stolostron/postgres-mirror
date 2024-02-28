171ad145f4e2c9b818d4eec753d5e61b86984172
Gurney Buchanan <gurney.buchanan@gmail.com>
2024-02-28 13:59:15 -0500

    Shallow single-branch clone for better performance

--- mirror_postgres.sh
+++ mirror_postgres.sh
@@ -12,40 +12,29 @@ DEST_URL_POSTGRES_13=quay.io/acm-d/postgresql-13
 
 # Functions
 mirror_postgres () {
-    git checkout $1
-    pushd extras
-    IMAGE_JSON=$(cat *.json | jq -r --arg version "$2" '.[] | select(.["image-key"] == $version)')
+    git clone --branch=$2 --depth=1 https://pkgs.devel.redhat.com/git/containers/$1
+    pushd $1/extras
+    IMAGE_JSON=$(cat *.json | jq -r --arg version "$3" '.[] | select(.["image-key"] == $version)')
     SOURCE_URL=$(echo $IMAGE_JSON | jq -r '.["image-remote"]')/$(echo $IMAGE_JSON | jq -r '.["image-name"]')@$(echo $IMAGE_JSON | jq -r '.["image-digest"]')
-    oc image mirror --keep-manifest-list=true $SOURCE_URL $3
+    oc image mirror --keep-manifest-list=true $SOURCE_URL $4
     popd
+    rm -rf $1
 }
 
 # Login
 podman login registry.redhat.io -u $REGISTRY_REDHAT_USER -p "$REGISTRY_REDHAT_PASSWORD"
 podman login quay.io -u $QUAY_USER -p $QUAY_PASSWORD
 
-# Get multicluster-engine-operator-bundle repo
-git clone https://pkgs.devel.redhat.com/git/containers/multicluster-engine-operator-bundle
-pushd multicluster-engine-operator-bundle
 # MCE
-mirror_postgres multicluster-engine-2.2-rhel-8 postgresql_12 $DEST_URL_POSTGRES_12
-mirror_postgres multicluster-engine-2.3-rhel-8 postgresql_12 $DEST_URL_POSTGRES_12
-mirror_postgres multicluster-engine-2.4-rhel-8 postgresql_12 $DEST_URL_POSTGRES_12
-mirror_postgres multicluster-engine-2.5-rhel-9 postgresql_12 $DEST_URL_POSTGRES_12
-mirror_postgres multicluster-engine-2.6-rhel-9 postgresql_12 $DEST_URL_POSTGRES_12
-# Clean up multicluster-engine-operator-bundle
-popd
-rm -rf multicluster-engine-operator-bundle
+mirror_postgres multicluster-engine-operator-bundle multicluster-engine-2.2-rhel-8 postgresql_12 $DEST_URL_POSTGRES_12
+mirror_postgres multicluster-engine-operator-bundle multicluster-engine-2.3-rhel-8 postgresql_12 $DEST_URL_POSTGRES_12
+mirror_postgres multicluster-engine-operator-bundle multicluster-engine-2.4-rhel-8 postgresql_12 $DEST_URL_POSTGRES_12
+mirror_postgres multicluster-engine-operator-bundle multicluster-engine-2.5-rhel-9 postgresql_12 $DEST_URL_POSTGRES_12
+mirror_postgres multicluster-engine-operator-bundle multicluster-engine-2.6-rhel-9 postgresql_12 $DEST_URL_POSTGRES_12
 
-# Get acm-operator-bundle repo
-git clone https://pkgs.devel.redhat.com/git/containers/acm-operator-bundle
-pushd acm-operator-bundle
-# MCE
-mirror_postgres rhacm-2.7-rhel-8 postgresql_13 $DEST_URL_POSTGRES_13
-mirror_postgres rhacm-2.8-rhel-8 postgresql_13 $DEST_URL_POSTGRES_13
-mirror_postgres rhacm-2.9-rhel-8 postgresql_13 $DEST_URL_POSTGRES_13
-mirror_postgres rhacm-2.10-rhel-9 postgresql_13 $DEST_URL_POSTGRES_13
-mirror_postgres rhacm-2.11-rhel-9 postgresql_13 $DEST_URL_POSTGRES_13
-# Clean up multicluster-engine-operator-bundle
-popd
-rm -rf acm-operator-bundle
+# ACM
+mirror_postgres acm-operator-bundle rhacm-2.7-rhel-8 postgresql_13 $DEST_URL_POSTGRES_13
+mirror_postgres acm-operator-bundle rhacm-2.8-rhel-8 postgresql_13 $DEST_URL_POSTGRES_13
+mirror_postgres acm-operator-bundle rhacm-2.9-rhel-8 postgresql_13 $DEST_URL_POSTGRES_13
+mirror_postgres acm-operator-bundle rhacm-2.10-rhel-9 postgresql_13 $DEST_URL_POSTGRES_13
+mirror_postgres acm-operator-bundle rhacm-2.11-rhel-9 postgresql_13 $DEST_URL_POSTGRES_13
