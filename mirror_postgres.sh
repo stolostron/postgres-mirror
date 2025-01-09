29800929429803d1cb8aafe592772e34270cea0d
Gurney Buchanan <gurney.buchanan@gmail.com>
2025-01-09 14:47:09 -0500

    Add brew mirroring option

--- mirror_postgres.sh
+++ mirror_postgres.sh
@@ -3,6 +3,8 @@
 # Must be set:
 # REGISTRY_REDHAT_USER - user with access to registry.redhat.io
 # REGISTRY_REDHAT_PASSWORD - password for user with access to registry.redhat.io
+# RH_BREW_REGISTRY_USERNAME - user with access to brew.registry.redhat.io
+# RH_BREW_REGISTRY_PASSWORD - password for user with access to registry.redhat.io
 # QUAY_USER - user with access to push to quay.io/acm-d
 # QUAY_PASSWORD - password for userr with access to push to quay.io/acm-d
 
@@ -18,6 +20,9 @@ mirror_external_images () {
     pushd $1/extras
     IMAGE_JSON=$(cat *.json | jq -r --arg version "$3" '.[] | select(.["image-key"] == $version)')
     SOURCE_URL=$(echo $IMAGE_JSON | jq -r '.["image-remote"]')/$(echo $IMAGE_JSON | jq -r '.["image-name"]')@$(echo $IMAGE_JSON | jq -r '.["image-digest"]')
+    if [[ "$6" != "" ]]; then
+        SOURCE_URL=$(echo $SOURCE_URL | sed -e "s/registry.redhat.io/$6/g")
+    fi
     echo "oc image mirror --registry-config=$5 --keep-manifest-list=true $SOURCE_URL $4"
     oc image mirror --registry-config=$5 --keep-manifest-list=true $SOURCE_URL $4
     popd
@@ -28,8 +33,9 @@ mirror_external_images () {
 AUTHFILE=$PWD/auth.json
 QUAY_AUTH="$(echo -n "$QUAY_USER:$QUAY_PASSWORD" | base64 -w 0)"
 REDHAT_AUTH="$(echo -n "$REGISTRY_REDHAT_USER:$REGISTRY_REDHAT_PASSWORD" | base64 -w 0)"
-jq --arg redhat_auth "$REDHAT_AUTH" --arg quay_auth "$QUAY_AUTH" --null-input -r --tab \
-    '{"auths":{"quay.io":{"auth":$quay_auth},"registry.redhat.io":{"auth":$redhat_auth}}}' > $AUTHFILE
+BREW_AUTH="$(echo -n "$RH_BREW_REGISTRY_USERNAME:$RH_BREW_REGISTRY_PASSWORD" | base64 -w 0)"
+jq --arg redhat_auth "$REDHAT_AUTH" --arg quay_auth "$QUAY_AUTH" --arg brew_auth $BREW_AUTH --null-input -r --tab \
+    '{"auths":{"quay.io":{"auth":$quay_auth},"registry.redhat.io":{"auth":$redhat_auth}, "brew.registry.redhat.io":{"auth":$brew_auth}}}' > $AUTHFILE
 
 # MCE
 mirror_external_images multicluster-engine-operator-bundle multicluster-engine-2.2-rhel-8 postgresql_12 $DEST_URL_POSTGRES_12 $AUTHFILE
@@ -40,8 +46,8 @@ mirror_external_images multicluster-engine-operator-bundle multicluster-engine-2
 mirror_external_images multicluster-engine-operator-bundle multicluster-engine-2.7-rhel-9 postgresql_12 $DEST_URL_POSTGRES_12 $AUTHFILE
 mirror_external_images multicluster-engine-operator-bundle multicluster-engine-2.8-rhel-9 postgresql_12 $DEST_URL_POSTGRES_12 $AUTHFILE
 
-mirror_external_images multicluster-engine-operator-bundle multicluster-engine-2.8-rhel-9 ose_cluster_api_rhel9 $DEST_URL_CAPI_RHEL9 $AUTHFILE
-mirror_external_images multicluster-engine-operator-bundle multicluster-engine-2.8-rhel-9 ose_aws_cluster_api_controllers_rhel9 $DEST_URL_CAPA_RHEL9 $AUTHFILE
+mirror_external_images multicluster-engine-operator-bundle multicluster-engine-2.8-rhel-9 ose_cluster_api_rhel9 $DEST_URL_CAPI_RHEL9 $AUTHFILE brew.registry.redhat.io
+mirror_external_images multicluster-engine-operator-bundle multicluster-engine-2.8-rhel-9 ose_aws_cluster_api_controllers_rhel9 $DEST_URL_CAPA_RHEL9 $AUTHFILE brew.registry.redhat.io
 
 # ACM
 mirror_external_images acm-operator-bundle rhacm-2.7-rhel-8 postgresql_13 $DEST_URL_POSTGRES_13 $AUTHFILE
